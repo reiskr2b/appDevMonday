4f6df7c33b6734a0afb1ab1c4753eb0913e1ac4e
FranciscoJadeEvans <jefrancisco@paterostechnologicalcollege.edu.ph>
2025-02-24 20:04:45 +0800

    Added name: Jade Evans E. Francisco

--- Activity1.java
+++ Activity1.java
@@ -28,7 +28,8 @@ public class Activity1 {
            System.out.println("Bryle O. Lacandula - BSIT 2A");
 	   System.out.println("Marco Kim Sean C. Mendiola - BSIT 2A");
 	   System.out.println("Joshua Mirandilla H. - BSIT-2B");
-	  System.out.println("Maria Theresa F. Alim - BSIT-2A");
+	   System.out.println("Maria Theresa F. Alim - BSIT-2A");
+	   System.out.println("Jade Evans E. Francisco - BSIT 2A");
 
     }
 }
