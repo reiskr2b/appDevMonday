39337776942ea03b7f22c826fab2251189b0a9ed
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-02-24 19:53:48 +0800

    Updated Activity1.java

--- Activity1.java
+++ Activity1.java
@@ -4,40 +4,20 @@ public class Activity1 {
    
         // Collaborators: Add your names below using the format:
         // System.out.println("Your Full Name - SECTION");
-
-
-	        System.out.println("Aliyah Gayle Jalique - BSIT 2A");
-       
-
+           System.out.println("Aliyah Gayle Jalique - BSIT 2A");
            System.out.println("Lance Whynzel U. David - BSIT-2A")
            System.out.println("Jener N. Demetrial - BSIT-2A");
            System.out.println("Nate Christian I. Walo - BSIT-2A");
            System.out.println("Stephen Louise C. Lanzar - BSIT-2B");
-
-	         System.out.println("Aaron San Agustin - BSIT-2A");
-
-
+           System.out.println("Aaron San Agustin - BSIT-2A");
            System.out.println("Louie Justine V. Luceor - BSIT-2A");
-
-
            System.out.println("Joel P. Lluz Jr. - BSIT-2A");
-
-
            System.out.println("Kiel Rivera - BSIT-2B");	
-
            System.out.println("Reign Myer F. Mendoza - BSIT 2A");
-
-          System.out.println("Ralph Allen M. Palomas - BSIT 2A");
-
+           System.out.println("Ralph Allen M. Palomas - BSIT 2A");
            System.out.println("Jerome F. Magno - BSIT 2B");
-
            System.out.println("Ramon G. Balce Jr. - BSIT 2B");
-
 	   System.out.println("Marco Kim Sean C. Mendiola - BSIT 2A");
-
-
-
-
 	   System.out.println("Joshua Mirandilla H. - BSIT-2B");
 
 
