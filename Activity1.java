90e5878121330bf4c55fde7f39ad6b3a5b8f6684
IannJayy <adrianjaylubrico8@gmail.com>
2025-02-24 20:15:09 +0800

    Update Activity1.java

--- Activity1.java
+++ Activity1.java
@@ -31,7 +31,7 @@ public class Activity1 {
 	   System.out.println("Joshua Mirandilla H. - BSIT-2B");
 	   System.out.println("Maria Theresa F. Alim - BSIT-2A");
 	   System.out.println("Jade Evans E. Francisco - BSIT 2A");
-
+	   System.out.println("Adrian Jay L. Gutierrez - BSIT 2A");
 
     }
 }
