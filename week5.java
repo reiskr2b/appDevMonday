1d0b351dae5d687d42fbef01be1fccbe6a67ae1d
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-03 18:36:47 +0800

    Create week5.java ORIGINAL FILE

---
+++ week5.java
@@ -0,0 +1,9 @@
+public class week5 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+       
+        // Call each student's method
+        // More students will add their method calls here...
+        //lastname.display();
+    }
+}
