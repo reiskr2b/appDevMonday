ccd7efc7446b4617bddcabd3849104e7d8b9db02
Maxenekhyle <msvelayo@paterostechnologicalcollege.edu.ph>
2025-03-03 20:33:25 +0800

    Adde Velayo.java with age calculation

--- Velayo.java/Velayo.java
+++ Velayo.java/Velayo.java
@@ -2,7 +2,7 @@ public class Velayo {
     public static void display() {
         int age = 20; // Replace with user input if necessary
         System.out.println("Hello! This is Velayo contributing to the project.");
-        System.out.println("My current age is: " + age);
+        System.out.println("My current age is:  " + age);
         System.out.println("Next year, I will be: " + (age + 1));
     }
 }
